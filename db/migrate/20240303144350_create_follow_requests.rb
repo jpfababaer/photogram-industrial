b8c82fce2c993b7b5ec78287862fdc2ead5744ab
John Paul Fababaer <100891759+jpfababaer@users.noreply.github.com>
2024-03-03 14:55:34 +0000

    Generated likes table

--- db/migrate/20240303144350_create_follow_requests.rb
+++ db/migrate/20240303144350_create_follow_requests.rb
@@ -1,7 +1,7 @@
 class CreateFollowRequests < ActiveRecord::Migration[7.0]
   def change
     create_table :follow_requests do |t|
-      #6 -> 
+      #6 -> _create_likes.rb 
       t.references :recipient, null: false, foreign_key: {to_table: :users}
       t.references :sender, null: false, foreign_key: {to_table: :users}
       t.string :status, default: "pending"
