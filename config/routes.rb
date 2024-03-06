13cd1787a9fc35d87da8bb2bfc5a364214351883
John Paul Fababaer <100891759+jpfababaer@users.noreply.github.com>
2024-03-06 22:31:00 +0000

    defined the users#show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  root to: "photos#index"
-  
+  root "photos#index"
+
   devise_for :users
 
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
+
+  get "/:username" => "users#show", as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 end
